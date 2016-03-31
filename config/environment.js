49640b44c373c72df3756a60bc3a853199b3f573
Austin Burdine <acburdine@gmail.com>
2016-03-31 15:26:45 -0500

    add ember-cli-deploy to push to gh-pages

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
     }
 
     if (environment === 'production') {
-
+        ENV.baseURL = '/olivet-bingo';
+        ENV.locationType = 'hash';
     }
 
     return ENV;
